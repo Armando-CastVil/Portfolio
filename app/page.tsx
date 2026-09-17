f12657b90bd888e76f3a56efece256272f1c1b97
Armando CastVil <Armando.CastVil@gmail.com>
2026-09-16 23:10:30 -0700

    added smashbase logo image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import Image from "next/image";
 import ProfilePicture from "../Assets/pictures/ProfilePicture.jpg";
+import smashbaseLogo from "../Assets/pictures/smashbaselogo.webp"
 export default function Home() {
 
   return (
@@ -260,7 +261,7 @@ export default function Home() {
             />
 
             <Project
-              image="/projects/data-analysis.jpg"
+              image="/"
               title="Smashbase.gg"
               description="Analyzed a dataset using Python and SQL to identify trends and created visualizations for better decision-making."
               tags={["Python", "SQL", "Pandas", "Matplotlib"]}
